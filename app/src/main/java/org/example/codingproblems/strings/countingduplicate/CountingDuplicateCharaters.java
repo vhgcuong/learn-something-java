fdf38648b895438ff85d7005d9ad4bd9646aeb3e
yotoko <vhgcuong95@gmail.com>
2026-03-22 23:46:53 +0700

    Counting duplicate characters

---
+++ app/src/main/java/org/example/codingproblems/strings/countingduplicate/CountingDuplicateCharaters.java
@@ -0,0 +1,9 @@
+package org.example.codingproblems.strings.countingduplicate;
+
+/**
+ * Counting duplicate characters: Write a program that counts duplicate characters from a given string.
+ * 
+ */
+public class CountingDuplicateCharaters {
+    
+}
